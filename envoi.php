950d8429540d88e46aff026d0dd6ae39f3789202
LeoLdc <ledocq.leo@laposte.net>
2018-05-22 16:07:55 +0200

    maj

--- envoi.php
+++ envoi.php
@@ -3,7 +3,7 @@ $nom=$HTTP_POST_VARS['nom'];
 $mail=$HTTP_POST_VARS['mail'];
 $objet=$HTTP_POST_VARS['objet'];
 $message=$HTTP_POST_VARS['message'];
-
+$sexe=$HTTP_POST_VARS['sexe']
 /////voici la version Mine
 $headers = "MIME-Version: 1.0\r\n";
 
@@ -14,10 +14,10 @@ $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 $headers .= "From: $nom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP";
 
 $subject="$objet";
-$destinataire="webmaster@votre-site.com"; //remplacez "webmaster@votre-site.com" par votre adresse e-mail
+$destinataire="ledocq.leo@laposte.net"; //remplacez "webmaster@votre-site.com" par votre adresse e-mail
 $body="$message";
-if (mail($destinataire,$subject,$body,$headers)) {
-echo "Votre mail a été envoyé<br>";
+if (mail($sexe,$destinataire,$subject,$body,$headers)) {
+echo "Votre mail a été envoyé";
 } else {
 echo "Une erreur s'est produite";
 }
@@ -25,5 +25,3 @@ echo "Une erreur s'est produite";
 <p align="center">Vous allez bientot etre redirigé vers la page d'acceuil<br>
 Si vous n'etes pas redirigé au bout de 5 secondes cliquez <a href="http://www.votre-site.com">ici
 </a></p>
-
- 
--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link rel="stylesheet" href="bootstrap.css">
   <meta charset="utf-8">
   <title>
       Formulaire De Contact.
   </title>
 </head>
+
 <body>
-  <p align="center"><strong>Contact</strong></p>
+  <div align="center" id="sexe">
+  <div align="left">homme</div></td>
+  <td colspan="2"><input type="radio" name="sexe" size="45" maxlength="100"></td>
+  <div align="left">femme</div></td>
+  <td colspan="2"><input type="radio" name="sexe" size="45" maxlength="100"></td>
+</div>
+  <p align="center"><strong><u>Contact</u></strong></p>
   <table width="500" border="0" align="center" cellpadding="0" cellspacing="0">
   <form action="envoi.php" method="post" enctype="application/x-www-form-urlencoded" name="formulaire">
   <tr>
-  <td colspan="3"><strong>Envoyer un message</strong></td>
+  <td colspan="3"><strong><div class="strong"><u>Envoyer un message</u></div></strong></td>
   </tr>
+  <hr/>
   <tr>
   <td><div align="left">Votre nom :</div></td>
   <td colspan="2"><input type="text" name="nom" size="45" maxlength="100"></td>
@@ -26,7 +35,7 @@
   <td colspan="2"><input type="text" name="objet" size="45" maxlength="120"></td>
   </tr>
   <tr>
-  <td><div align="left">Message : </div></td>
+  <td><div align="left">Message: </div></td>
   <td colspan="2"><textarea name="message" cols="50" rows="10"></textarea></td>
   </tr>
   <tr>
@@ -41,7 +50,8 @@
   </form>
   </table>
 </body>
+
 <footer>
-  <p>h</p>
+
 </footer>
 </html>
