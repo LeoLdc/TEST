104e81208f749a17ce271c469b5c3ce4ae1d7681
LeoLdc <ledocq.leo@laposte.net>
2018-05-22 14:43:06 +0200

    maj

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="utf-8">
+  <title>
+      Formulaire De Contact.
+  </title>
+</head>
+<body>
 <form id="contact" method="post" action="envoi.php">
                 <p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" tabindex="1" /></p>
         <p><label for="email">Email :</label><input type="text" id="email" name="email" tabindex="2" /></p>
@@ -8,4 +15,8 @@
 
     <div style="text-align:center;"><input type="submit" name="envoi" value=" Envoyer " /></div>
 </form>
+</body>
+<footer>
+  <p>h</p>
+</footer>
 </html>
